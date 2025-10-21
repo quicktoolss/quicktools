efb29a58ee133e077d6afd12f982a69d3916391c
quicktoolss <sammysoham2@gmail.com>
2025-10-21 14:03:40 +0530

    Update script.js

--- tools/image-background-remover/script.js
+++ tools/image-background-remover/script.js
@@ -20,7 +20,7 @@ async function loadModel() {
   if (!model) {
     progressContainer.classList.remove("hidden");
     updateProgress(10, "Loading AI model...");
-    model = await deeplab.load({ base: 'pascal', quantizationBytes: 2 });
+    model = await deeplab.load({ base: 'ade20k', quantizationBytes: 4 });
     updateProgress(30, "Model loaded");
   }
   return model;
